fa38e8acc3535fae88f1e5322a077a9198aa3c4f
Elahe Hashemi <elahe.hs481@gmail.com>
2023-07-24 21:31:09 +0330

    main components added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Route , Switch , Link, Redirect } from 'react-router-dom';
+
+class App extends Component {
+
+
+
+    render() {
+        return (
+            <div>
+
+
+            </div>
+        );
+    }
+}
+
+export default App;
